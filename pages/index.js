9460a9956fdb9714a99925cc0a788c9c5682b764
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2021-04-01 17:03:45 +0530

    Created first api route

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
-
+import {useRef} from "react";
 
 export default function Home() {
-  return (
-   <div>
-     <h1>Home</h1>
-    </div>
-  )
+    const emailIn = useRef()
+    const messageRf = useRef();
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const email = emailIn.current.value;
+        const message = messageRf.current.value;
+        const reqBody = {
+            email,
+            message
+        }
+        fetch('/api/feedback',
+            {
+                method: 'POST',
+                body: JSON.stringify(reqBody),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json())
+            .then(data => console.log(data))
+
+    }
+    return (
+        <div>
+            <h1>Home</h1>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <input ref={emailIn} type="email" placeholder={'Email'} id={'email'}/>
+                </div>
+                <div>
+                    <textarea ref={messageRf} placeholder={'feedback'} id={'feedback'}/>
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    )
 }
 
